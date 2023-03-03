8c0de0d2303f34845bc65741837aafff9a8a0513
Ibrahima FAYE <ibrahima.faye@fahpo.com>
2023-03-03 20:20:02 +0100

    First commit

---
+++ src/test/java/com/fagenius/SpringGithubactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
